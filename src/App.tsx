cfe249bd1468d8833c8b3d8053e62e5e940a58f3
Vanessa <vantaidesign8@gmail.com>
2024-05-28 21:34:48 +0100

    Defined a type for Props with `children`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import DietGoal from './components/DietGoal.tsx';
 
+// JSX Code: the paragraph element is the children and we define the type for it in the DietGoal.tsx component file.
 export default function App() {
   return (
     <main>
-      <DietGoal
-        title='Set and manage your diet goals'
-        description='Loose weight and get fit'
-      />
+      <DietGoal title='Set and manage your diet goals'>
+        <p>Loose weight and get fit</p>
+      </DietGoal>
     </main>
   );
 }
--- src/components/DietGoal.tsx
+++ src/components/DietGoal.tsx
@@ -1,19 +1,27 @@
-// Outsource (refactored) the type to make the code more readable and more extensible.
-interface DietGoalProps {
-  title: string;
-  description: string;
-}
+// This is a type not an object or func etc.
+// `import { type ReactNode } from 'react';`
+import { type PropsWithChildren } from 'react';
+
+/* Outsource (refactored) the type to make the code more readable and more extensible. The code: */
+// The children prop is of React type
+// interface DietGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+/* Another way to define our type, use the `PropsWithChildren` type with title prop and is the type of string. */
+type DietGoalProps = PropsWithChildren<{ title: string }>;
 
 /* Define the type for React`props` parameter.
 Connect - up from the `title` and `description` from the return statement to the props parameters.
 Use the `DietGoalProps` as a type for the Props object since its already defined above this func.
  */
-export default function DietGoal({ title, description }: DietGoalProps) {
+export default function DietGoal({ title, children }: DietGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
+        {children}
       </div>
       <button>Delete</button>
     </article>
