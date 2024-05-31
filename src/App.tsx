44212d511986c0aff5a005d53edfee26266579da
Vanessa T <tai.v.tsang@gmail.com>
2024-05-31 18:49:10 +0100

    feat: implemented hook to make button click render UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
+
+import { useState } from 'react';
 import DietGoal from './components/DietGoal.tsx';
 import Header from './components/Header.tsx';
 import goalsImg from './assets/goals.jpg';
 
-// JSX syntax: the paragraph element is the children and we define
-// the type for it in the DietGoal.tsx component file.
+// Define our type for `DietGoal` 
+type DietGoal = {
+  title: string;
+  description: string;
+  id: number;
+}
+
+// useState() func hook to generate new goals and dynamically display them on the screen.
+// Alternative syntax is the default Array type: `const [goals, setGoals] = useState<Arry<DietGoal>>([]);`
 export default function App() {
+  const [goals, setGoals] = useState<DietGoal[]>([]);
+
+// logic for manipulating empty Array inside useState([]) func with the help of `setGoals`.
+  function handleAddGoal() {
+    setGoals();
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Diet Goals</h1>
       </Header>
+      <button onClick={handleAddGoal()}>Add Goal</button>
       <DietGoal title='Set and manage your diet goals'>
         <p>Loose weight and get fit</p>
       </DietGoal>
--- src/components/DietGoal.tsx
+++ src/components/DietGoal.tsx
@@ -1,20 +1,12 @@
 // This is a type not an object or func etc.
-// `import { type ReactNode } from 'react';`
 import { type PropsWithChildren } from 'react';
-
-/* Outsource (refactored) the type to make the code more readable and more extensible. The code: */
-// The children prop is of React type
-// interface DietGoalProps {
-//   title: string;
-//   children: ReactNode;
-// }
 
 /* Another way to define our type, use the `PropsWithChildren` type with title prop and is the type of string. */
 type DietGoalProps = PropsWithChildren<{ title: string }>;
 
-/* Define the type for React`props` parameter.
+/* Define the type for React `props` parameter.
 Connect - up from the `title` and `description` from the return statement to the props parameters.
-Use the `DietGoalProps` as a type for the Props object since its already defined above this func.
+Use the `DietGoalProps` as a type for the Props object since its already defined above.
  */
 export default function DietGoal({ title, children }: DietGoalProps) {
   return (
